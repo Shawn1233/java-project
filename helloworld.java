3fec59348fae5e44db334a451e07ae710058f7b0
Shawn1233 <38486594+Shawn1233@users.noreply.github.com>
2018-04-18 11:48:31 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -19,8 +19,28 @@ class helloworld
  		 System.out.print(" "+n3);    
  		 n1=n2;    
 	  	n2=n3;    
-		 }    
+		 }
+		int i, j;
+		int n=5;
+ 
+        // outer loop to handle number of rows
+        //  n in this case
+        for(i=0; i<n; i++)
+        {
+ 
+            //  inner loop to handle number of columns
+            //  values changing acc. to outer loop  
+            for(j=0; j<=i; j++)
+            {
+                // printing stars
+                System.out.print("* ");
+            }
+ 
+            // ending line after each row
+            System.out.println();
+        }
+		
   
  
-	}
+	
 }
